fd4bb7cfb2a82cd526566f39ab972e4f34cadba5
Lukasz Kozuchowski <Lukasz.Kozuchowski@gmail.com>
2017-02-14 15:26:47 +0100

    Delete elpa dir content

--- init.el
+++ init.el
@@ -9,7 +9,9 @@
 (add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("gnu" . "http://elpa.gnu.org/packages/") t)
 
 ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 ;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
